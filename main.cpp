d62be7217a9f4cc74e5701a4e3af42179d1c123d
Camilo <camilo.pinedaserna@heig-vd.ch>
2016-03-15 11:03:48 +0100

    first commit création git

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: camilo
+ *
+ * Created on 15. mars 2016, 11:01
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+   return 0;
+}
+

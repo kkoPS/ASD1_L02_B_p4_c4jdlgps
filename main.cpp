07258636811b4c2f447e53f0688b1daa8e55d706
Rémi <Rmi@REMI-SURFACE.einet.ad.eivd.ch>
2016-04-05 18:48:36 +0200

    Ajout de l'interface utilisateur

--- main.cpp
+++ main.cpp
@@ -7,26 +7,138 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <limits>
+#include <ctime>
 
 #include "P4cmr.h"
 
 using namespace std;
 
-/*
- * 
- */
-int main(int argc, char** argv) {
-   cout << "hello world" << endl;
-   
-   P4cmr board;
+// BASE SUR LE TIC TAC TOE 
+//
+// choisit interactivement le prochain mouvement
+// en le demandant au joueur et en verifiant que
+// le choix est licite.
+// cette fonction est recursive si le choix est
+// illicite.
+int interactive(Player player, const P4cmr& p4Game) {
+    cout << endl << "Prochain mouvement du joueur " << ((player==X) ? "X" : "O" ) << ": " << flush;
+    
+    int n;
+    cin >> n;
+    cout << endl;
+    
+    if(!cin.good() || !p4Game.isValidMove(n - 1)) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(),'\n');
+        cout << "Mouvement non valide. Essayez encore." << endl;
+        return interactive(player, p4Game);
+    } else
+        return n - 1;
+}
 
-   board.playInColumn(3, O);
+// indique si le tableau est plein
+// (les cases du haut sont remplies)
+bool isBoardFull(P4cmr p4Game) 
+{
+   for (int cols=0; cols < 7; cols++)
+   {
+      if (p4Game.isValidMove(cols)) //On regarde si les colonnes sont remplies
+      {
+         return false;
+      }
+   }
+   return true;
+}
+
+
+// choisit interactivement le nombre de joueurs humains
+// en le demandant au joueur et en verifiant que
+// le choix est licite.
+// cette fonction est recursive si le choix est
+// illicite.
+int askNumberOfPlayers() {
+    
+    cout << "Combien de joueurs humains? (0,1 ou 2) " << flush;
+    
+    int n;
+    cin >> n;
+    cout << endl;
+    
+    if(!cin.good() || (n < 0) || (n>2) ) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(),'\n');
+        cout << "Nombre de joueur invalide. " << endl;
+        return askNumberOfPlayers();
+    }
+    else
+        return n;
+}
+
+
+int main() 
+{   
+   P4cmr p4Game;
+   p4Game.reset();
+   Player currentPlayer = Player::O;
+   bool human[2] = {1, 1};
+   
+   srand ( (int) time(0) );
+    
+   int np = askNumberOfPlayers();
+
+   switch(np) 
+   {
+      case 0:
+         human[0] = human[1] = false;
+         break;
+      case 1:
+         human[0] = rand() % 2;  // choix aleatoire du premier joueur
+         human[1] = !human[0];
+         break;
+      case 2:
+         human[0] = human[1] = true;
+         break;
+   }   
+   
+   while(!p4Game.isWinner(O) && !p4Game.isWinner(X) && !isBoardFull(p4Game)) 
+   {
+      p4Game.printBoard(cout);
+
+      int nextMove;
+      if (human[(currentPlayer + 1) / 2])
+          nextMove = interactive(currentPlayer, p4Game);
+      else {
+          nextMove = p4Game.chooseNextMove(currentPlayer, 2);
+          cout << endl << "AI joue " << nextMove << endl;
+      }
+      p4Game.playInColumn(nextMove, currentPlayer);
+      
+      currentPlayer = (Player)(-currentPlayer);
+   }
+   
+   p4Game.printBoard(cout);   
+   
+   if (p4Game.isWinner(X) || p4Game.isWinner(O)) //Si l'un des joueurs a gagne
+   {      
+      cout << " \n\n" << (p4Game.isWinner(X) ? 'X' : 'O') << " gagne! \n\n\n";
+   }
+   else
+   {
+      cout << " \n\n Egalité! \n\n\n";
+   }
+
+   return EXIT_SUCCESS;
+
+   
+   /*
+   p4Game.playInColumn(3, O);
  
    
-   board.play(4, 1, X);   
-   board.play(3, 2, O);
-   board.play(2, 3, X);
-   board.play(1, 4, X);
+   p4Game.play(4, 1, X);   
+   p4Game.play(3, 2, O);
+   p4Game.play(2, 3, X);
+   p4Game.play(1, 4, X);
    
    //   board.play(0,0,X);
 //   board.play(0,2,O);
@@ -38,11 +150,10 @@ int main(int argc, char** argv) {
 //   board.play(5,1,O);
 //   board.play(6,1,X);
    
-   board.printBoard(cout);
+   p4Game.printBoard(cout);
    
    cout << boolalpha << 
-           board.isWinner(X) << endl;
+           p4Game.isWinner(X) << endl;*/
 
-   return 0;
 }
 
